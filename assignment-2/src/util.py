fe7d9e7c90cbf9a4f532306121ab73cd5444db90
Tomas Kostadinov <github@kostadinov.it>
2023-10-20 13:59:14 +0200

    clean up code

--- assignment-2/src/util.py
+++ assignment-2/src/util.py
@@ -50,3 +50,49 @@ def generate_confusion_matrix(conf_matrix, file_name, y_test, title):
     plt.tight_layout()
     plt.savefig("confusion_matrices/" + file_name, dpi=300)
     plt.show()
+
+
+def downsampling(X, y):
+    # Define the desired maximum instances per class
+    max_instances_per_class = 1000
+
+    # Initialize lists to store downsampled data
+    downsampled_X = []
+    downsampled_y = []
+    # Iterate through unique classes
+    unique_classes = np.unique(y)
+    for class_label in unique_classes:
+        # Get indices of instances for this class
+        class_indices = np.where(y == class_label)[0]
+
+        # Randomly select up to max_instances_per_class instances for this class
+        if len(class_indices) > max_instances_per_class:
+            selected_indices = np.random.choice(
+                class_indices, max_instances_per_class, replace=False
+            )
+        else:
+            selected_indices = class_indices
+
+        # Append the selected instances to the downsampled data
+        downsampled_X.extend(X[selected_indices])
+        downsampled_y.extend(y[selected_indices])
+
+    # Convert the downsampled data to NumPy arrays
+    downsampled_X = np.array(downsampled_X)
+    downsampled_y = np.array(downsampled_y)
+
+    # Shuffle the downsampled data
+    shuffle_indices = np.random.permutation(len(downsampled_X))
+    downsampled_X = downsampled_X[shuffle_indices]
+    downsampled_y = downsampled_y[shuffle_indices]
+
+    # Print the counts for each class in the downsampled dataset
+    unique_classes_downsampled, class_counts_downsampled = np.unique(
+        downsampled_y, return_counts=True
+    )
+    return (
+        downsampled_X,
+        downsampled_y,
+        unique_classes_downsampled,
+        class_counts_downsampled,
+    )
